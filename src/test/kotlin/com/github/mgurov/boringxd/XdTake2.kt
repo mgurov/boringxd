19f89c1c3c1fb71c4b0d86523dbbc8315aa77c7c
Mykola Gurov <mgurov@bol.com>
2018-03-15 18:51:05 +0100

    fine

--- src/test/kotlin/com/github/mgurov/boringxd/XdTake2.kt
+++ src/test/kotlin/com/github/mgurov/boringxd/XdTake2.kt
@@ -6,7 +6,7 @@ class XdTake2 : Xd {
     override fun receive(update: BoringTotals, message: String): Int {
         checkTotalsDoNotDecrease(steps.lastOrNull()?.boring, update)
 
-        val step = Step(
+        val step = makeNextStep(
                 boring = update,
                 previous = Previous(steps.lastOrNull())
         )
@@ -37,34 +37,47 @@ data class Previous constructor(
 
 data class Step(
         val boring: BoringTotals,
-        val previous: Previous
+        val previous: Previous,
+        val coverLostStock: Int,
+        val coverNewRequests: Int,
+        val delta: Int
 ) {
+    override fun toString(): String {
+        return "$boring, previous=$previous, coverLostStock=$coverLostStock, coverNewRequests=$coverNewRequests, delta=$delta"
+    }
+}
+
+fun makeNextStep(
+    boring: BoringTotals,
+    previous: Previous
+): Step {
     val coverLostStock: Int
     val coverNewRequests: Int
     val delta: Int
 
-    init {
+    val currentSupply = boring.supply()
 
-        val currentSupply = boring.supply()
-
-        if (previous.supply > currentSupply && currentSupply < previous.total) {
-            //stock lost and it affects previously assumed to be enough
-            coverLostStock = Integer.min(previous.total, previous.supply) - currentSupply
-        } else {
-            coverLostStock = 0
-        }
-
-        val newRequests = boring.total - previous.total
-        if (newRequests < 0) {
-            throw IllegalStateException("Unexpected negative requests $newRequests")
-        }
-
-        coverNewRequests = Math.min(newRequests, boring.shortage())
-
-        delta = coverLostStock + coverNewRequests
+    if (previous.supply > currentSupply && currentSupply < previous.total) {
+        //stock lost and it affects previously assumed to be enough
+        coverLostStock = Integer.min(previous.total, previous.supply) - currentSupply
+    } else {
+        coverLostStock = 0
     }
 
-    override fun toString(): String {
-        return "$boring, previous=$previous, coverLostStock=$coverLostStock, coverNewRequests=$coverNewRequests, delta=$delta"
+    val newRequests = boring.total - previous.total
+    if (newRequests < 0) {
+        throw IllegalStateException("Unexpected negative requests $newRequests")
     }
+
+    coverNewRequests = Math.min(newRequests, boring.shortage())
+
+    delta = coverLostStock + coverNewRequests
+
+    return Step(
+        boring = boring,
+        previous = previous,
+        coverLostStock = coverLostStock,
+        coverNewRequests = coverNewRequests,
+        delta = delta
+    )
 }
